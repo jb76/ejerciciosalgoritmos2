8c17d88f565754d45fd38386804f7555aa8bbaed
jb76 <jorgeb1976@outlook.com>
2016-08-06 11:10:39 -0600

    RadioPerimetro

---
+++ RadioPerimetro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radio;
+
+            Console.Write("Ingrese radio:");
+            radio = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("El perimetro del circulo es {0}", 3.1416 * (radio * 2));
+            Console.Write("El area del circulo es {0}", 3.1416 * radio * radio);
+            Console.ReadLine();
+
+        }
+    }
+}
