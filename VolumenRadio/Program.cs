e27da3f651c7dc5f54956b67cc71301cc510759e
jb76 <jorgeb1976@outlook.com>
2016-08-06 11:48:04 -0600

    VolumenRadio

---
+++ VolumenRadio/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumenRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        double pi, radio, area, volumen, longitud;
+            pi = 3.14;
+            Console.Write("Ingrese el radio de la circunferencia:");
+            radio = int.Parse(Console.ReadLine());
+            longitud = 2 * pi * radio;
+            area = Math.Round(pi * Math.Pow(radio, 2), 2);
+            volumen = Math.Round((4 / 3) * pi * Math.Pow(radio, 3), 2);
+            Console.WriteLine("");
+            Console.WriteLine("Longitud de la circunferencia:" + longitud);
+            Console.WriteLine("Area de la circunferencia: " + area);
+            Console.WriteLine("Volumen de la circunferencia: " + volumen);
+            Console.ReadLine();
+
+        }
+    }
+}
